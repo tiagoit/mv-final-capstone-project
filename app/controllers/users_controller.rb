715e405530d27197ff009f3355bcd7111ebb67dc
Tiago Ferreira (ferrreirati) <tiferreira12@gmail.com>
2020-01-19 15:30:19 -0300

    feat: auto login after register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: { user: @user }, status: :created
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                     user: { name: @user.name, email: @user.email } }, status: :created
     else
       render json: { errors: @user.errors.full_messages },
              status: :unprocessable_entity
