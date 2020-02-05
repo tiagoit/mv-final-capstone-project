2fd7fe557631c9b6a2d3d26139db66f9840325b9
Tiago Ferreira (tiagoit) <tiferreira12@gmail.com>
2020-02-05 16:04:28 -0300

    fix: remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, param: :_email
+  resources :users, param: :_email, only: [:create]
   resources :favourites, only: [:index, :create, :destroy]
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
